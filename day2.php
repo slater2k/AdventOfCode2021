b2598fdda822cd53180ae3b3dd4e356e41e6bcd9
slater2k <79055889+slater2k@users.noreply.github.com>
2021-12-05 20:10:54 +0000

    Day 2 part 2

--- day2.php
+++ day2.php
@@ -1,31 +1,47 @@
 <?php
 
 //--- Day 2: Dive! ---
-$commands = file_get_contents(__DIR__.'/inputs/day2');
-$commands_array = explode(PHP_EOL, $commands);
 
-$positions = [
-    'horizontal' => 0,
-    'depth' => 0
-];
+/**
+ * @param false $calculateDepthUsingAim
+ * @return int[]
+ */
+function getPositions(bool $calculateDepthUsingAim = false): array {
 
-foreach($commands_array as $command) {
+    $commands = file_get_contents(__DIR__.'/inputs/day2');
+    $commands_array = explode(PHP_EOL, $commands);
 
-    $instructions = explode(" ", $command);
-    $direction = $instructions[0];
-    $steps = $instructions[1];
+    $positions = [
+        'horizontal' => 0,
+        'depth' => 0,
+        'aim' => 0
+    ];
 
-    switch($direction) {
-        case 'forward':
-            $positions['horizontal'] += $steps;
-            break;
-        case 'down':
-            $positions['depth'] += $steps;
-            break;
-        case 'up':
-            $positions['depth'] -= $steps;
-            break;
+    foreach($commands_array as $command) {
+
+        $instructions = explode(" ", $command);
+        $direction = $instructions[0];
+        $steps = $instructions[1];
+
+        switch($direction) {
+
+            case 'forward':
+                $positions['horizontal'] += $steps;
+                $positions['depth'] += $calculateDepthUsingAim ? ($positions['aim'] * $steps) : 0;
+                break;
+            case 'down':
+                 $positions[$calculateDepthUsingAim ? 'aim' : 'depth'] += $steps;
+                break;
+            case 'up':
+                $positions[$calculateDepthUsingAim ? 'aim' : 'depth'] -= $steps;
+                break;
+        }
     }
+
+    return $positions;
 }
 
-echo $positions['horizontal'] * $positions['depth'];
+$partOne = getPositions();
+$partTwo = getPositions(true);
+
+echo "Part One: " . $partOne['horizontal'] * $partOne['depth'] . ", Part Two: " . $partTwo['horizontal'] * $partTwo['depth'];
