087a5506ef95cc78182d2525a02777422f7d3a48
slater2k <79055889+slater2k@users.noreply.github.com>
2021-12-05 19:05:50 +0000

    Day 2 part 1

--- day2.php
+++ day2.php
@@ -1,1 +1,31 @@
 <?php
+
+//--- Day 2: Dive! ---
+$commands = file_get_contents(__DIR__.'/inputs/day2');
+$commands_array = explode(PHP_EOL, $commands);
+
+$positions = [
+    'horizontal' => 0,
+    'depth' => 0
+];
+
+foreach($commands_array as $command) {
+
+    $instructions = explode(" ", $command);
+    $direction = $instructions[0];
+    $steps = $instructions[1];
+
+    switch($direction) {
+        case 'forward':
+            $positions['horizontal'] += $steps;
+            break;
+        case 'down':
+            $positions['depth'] += $steps;
+            break;
+        case 'up':
+            $positions['depth'] -= $steps;
+            break;
+    }
+}
+
+echo $positions['horizontal'] * $positions['depth'];
